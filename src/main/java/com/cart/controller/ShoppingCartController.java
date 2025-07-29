2955f18dd10ad16281ce0bb8702bdd8dac568aec
asankaperera <asanka.wpp@gmail.com>
2025-07-28 20:59:10 -0600

    added TODO.txt to .gitignore

--- src/main/java/com/cart/controller/ShoppingCartController.java
+++ src/main/java/com/cart/controller/ShoppingCartController.java
@@ -1,10 +1,14 @@
 package com.cart.controller;
 
 import com.cart.dto.BookDTO;
+import com.cart.model.Book;
 import com.cart.service.BookService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
@@ -18,12 +22,30 @@ public class ShoppingCartController {
     }
 
     @GetMapping("/books")
-    public List<BookDTO> listBooks() {
-        return bookService.getAllBooks();
+    public ResponseEntity<List<BookDTO>> listBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/books/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
     }
 
     @PostMapping("/books")
-    public void addBook(@RequestBody BookDTO bookDTO) {
-        bookService.addBook(bookDTO);
+    public ResponseEntity<BookDTO> addBook(@RequestBody BookDTO bookDTO) {
+        BookDTO book = bookService.addBook(new Book(bookDTO.getTitle(),
+                bookDTO.getAuthor(),
+                bookDTO.getIsbn(),
+                bookDTO.getPublicationYear(),
+                bookDTO.getPrice()));
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(book.getId())
+                .toUri();
+
+        return ResponseEntity.created(location)
+                .body(book);
     }
 }
